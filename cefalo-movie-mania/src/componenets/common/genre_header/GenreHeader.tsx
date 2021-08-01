8f77ed920137e983e029ec90522c319e79bd1ac6
Mehadi <mehadicse38@gmail.com>
2021-08-01 15:10:57 +0600

    PHASE2 : Added redirect to genre details displaying sorted movie list.

--- cefalo-movie-mania/src/componenets/common/genre_header/GenreHeader.tsx
+++ cefalo-movie-mania/src/componenets/common/genre_header/GenreHeader.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './_genreHeader.scss';
 
 interface GenreHeaderProps {
     id: number;
@@ -7,8 +9,12 @@ interface GenreHeaderProps {
 
 const GenreHeader: React.FC<GenreHeaderProps> = (genreHeaderProps: GenreHeaderProps) => {
     return (
-        <div>
-            <h1> {genreHeaderProps.name}</h1>
+        <div className="genre-header">
+            <h1>
+                <Link className="genre-name" to={`/genre/${genreHeaderProps.id}`}>
+                    #{genreHeaderProps.name}
+                </Link>
+            </h1>
         </div>
     );
 };
--- cefalo-movie-mania/src/pages/genre_details/GenreDetails.tsx
+++ cefalo-movie-mania/src/pages/genre_details/GenreDetails.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MoviesService from '../../services/movies/MoviesService';
+import { MoviesData } from '../../data_model/movies/MoviesData';
 import './_genreDetails.scss';
+import { cid, useInject } from 'inversify-hooks';
+import { useParams } from 'react-router-dom';
+import { MovieItem } from '../../data_model/commonData/MovieItem';
+import MovieGridItem from '../../componenets/common/movie_grid_item/MovieGridItem';
 
 const GenreDetails: React.FC = () => {
-    // const parsed = queryString.parse(location.search);
-    // console.log(parsed);
+    const { genreId } = useParams<{ genreId: string }>();
+    const [genreMovieList, setGenreMovieList] = useState<MovieItem[]>([]);
+    const [isGettingError, setIsGettingError] = useState<boolean>(false);
+    const [moviesService] = useInject<MoviesService>(cid.MoviesService);
+    const FetchMovieInfoAsync = (genreID: number, pageNumber: number): Promise<MoviesData> => {
+        return moviesService.getData(genreID, pageNumber);
+    };
+
+    const GetMovieByPopularity = (movies: MovieItem[]): MovieItem[] => {
+        return movies.sort((movieItem, comparedMovieItem) =>
+            movieItem.popularity < comparedMovieItem.popularity
+                ? 1
+                : comparedMovieItem.popularity < movieItem.popularity
+                ? -1
+                : 0,
+        );
+    };
+
+    useEffect(() => {
+        FetchMovieInfoAsync(Number(genreId), 1)
+            .then((moviesResponse: MoviesData) => {
+                setGenreMovieList(GetMovieByPopularity(moviesResponse.results));
+            })
+            .catch(error => {
+                console.log('Get error:-', error);
+                setIsGettingError(true);
+            });
+    }, []);
     return (
         <div className="genre-content">
-            <h1>Welcoem to Genre</h1>
+            {isGettingError ? (
+                <div className="error-message"> No movie found.</div>
+            ) : (
+                <div className="movie-details-by-popularity container">
+                    {genreMovieList.map(item => {
+                        return (
+                            <div key={item.id} className="movie-grid-item">
+                                <MovieGridItem movies={item} />
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 };
